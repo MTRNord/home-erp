eaf1503caa55400c6b70996a770a281fb81cb147
MTRNord <mtrnord1@gmail.com>
2023-10-21 20:22:13 +0200

    Improve static analysis and fix some bugs

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -15,7 +15,7 @@ class Room extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<string>
      */
     protected $fillable = ['name', 'description'];
 
